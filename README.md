09fe6b27d733f10c3dc86267c4f722b6f6448755
Angelo Capasso <capsangelo@gmail.com>
2021-11-14 19:27:36 +0100

    add changelog

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# goosegame
+# GooseGame
+
+L'applicazione implementata permette di poter giocare al gioco dell'oca. Non è un eseguibile ma fornisce una serie di funzionalità per poter essere integrate da sistemi esterni.
+
+Per poter effettuare la build basta lanciare il comando: mvn install -f pom.xml
+
+Durante la build verranno effettuati alcuni test automatici ed il lancio di alcune partite con un numero ben pre-definito di mosse.
+
+## Funzionalità
+
+Lo strumento espone una serie di metodi da poter essere utilizzati per muovere le pedine e aggiungere eventuali giocatori.
+
+In particolare, lo strumento fornisce i metodi per:
+
+- lanciare i dati;
+- eseguire la mossa rispetto ai dati lanciati;
+- aggiungere un giocatore.
--- src/main/java/it/stapedev/App.java
+++ src/main/java/it/stapedev/App.java
@@ -1,5 +1,8 @@
 package it.stapedev;
 
+import it.stapedev.data.Player;
+import it.stapedev.service.GooseLogics;
+
 /**
  * Hello world!
  */
@@ -9,9 +12,18 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GooseLogics game = new GooseLogics();
+        Player player1 = game.addPlayer("Player1");
+        Player player2 = game.addPlayer("Player2");
+        Player player3 = game.addPlayer("Player3");
+        for (int i = 1; i < 100; i++) {
+            game.move(player1, game.lauchDice(player1));
+            game.move(player2, game.lauchDice(player2));
+            game.move(player3, game.lauchDice(player3));
+        }
     }
 }
